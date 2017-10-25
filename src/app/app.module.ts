48f05ec320245bdf63aacaec81c8422798d8e9f1
Sirawit Tharana <sithrawi@yahoo.com>
2017-10-25 23:18:10 +0700

    Add angular round 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'; 
+
+import { GetphotoService } from './services/getphoto.service'
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+
+const appRoutes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'about', component: AboutusComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [GetphotoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
